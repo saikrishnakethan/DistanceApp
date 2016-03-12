e1e16d3bbbd00bcd55f701b49104b8aa37ef0157
kethan <mskethan@gmail.com>
2016-03-13 00:41:30 +0530

    Added Comments

--- app/src/main/java/com/example/kobe24/distancecalculator/MainActivity.java
+++ app/src/main/java/com/example/kobe24/distancecalculator/MainActivity.java
@@ -14,23 +14,22 @@ public class MainActivity extends Activity implements LocationListener{
     protected LocationManager mLocationManager;
     protected LocationListener locationListener;
     protected Context context;
-    double dist=0.0;
-    long ans=0;
-    Location prev;
+    double dist=0.0; // total distance travelled in double
+    long ans=0;      // total distance travelled in long , used to display answer
+    Location prev;   // last location recorded
     Location bestLocation=null;
     TextView txtLat;
 //    TextView txtLat1,txtLat2;
 
     private void getLastKnownLocation() {
-        List<String> providers = mLocationManager.getAllProviders();
+        List<String> providers = mLocationManager.getAllProviders(); // all types from which data can be obtained like GSM,CDMA
         for (String provider : providers) {
             Location l = mLocationManager.getLastKnownLocation(provider);
             if (l == null) {
                 continue;
             }
             if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
-                // Found best last known location: %s", l);
-                bestLocation = l;
+                bestLocation = l; // best location
                 prev=bestLocation;
             }
         }
@@ -40,19 +39,22 @@ public class MainActivity extends Activity implements LocationListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtLat = (TextView) findViewById(R.id.textview1);
+        txtLat = (TextView) findViewById(R.id.textview1); // textview where the answer is displayed
 //        txtLat1 = (TextView) findViewById(R.id.textview2);
 //        txtLat2= (TextView) findViewById(R.id.textview3);
         mLocationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 2, new LocationListener() {
+            // request listener with following parameters
+            // GPS , 2sec interval , minimum distance for recording change
             @Override
             public void onLocationChanged(Location location) {
-                dist += prev.distanceTo(location);
+                // function is called when Location is changed
+                dist += prev.distanceTo(location); // Distance between two locations in meters,float
                 ans = (long) dist;
 //                txtLat.setText(prev.getLongitude()+" "+prev.getLatitude());
 //                txtLat1.setText(location.getLongitude()+" "+location.getLatitude());
-                txtLat.setText("Distance Travelled : " + ans/1000 +" kms " + ans%1000 + " meter(s)");
-                prev = location;
+                txtLat.setText("Distance Travelled : " + ans/1000 +" kms " + ans%1000 + " meter(s)"); // Displaying
+                prev = location; // changing current location as previous location for next iteration
             }
 
             @Override
@@ -71,8 +73,8 @@ public class MainActivity extends Activity implements LocationListener{
                 // TODO Auto-generated method stub
             }
         });
-        getLastKnownLocation();
-        txtLat.setText("Distance Travelled : " + ans/1000 +"kms" + ans%1000 + " meter(s)");
+        getLastKnownLocation(); // get the best distance and initialise previous distance
+        txtLat.setText("Distance Travelled : " + ans/1000 +" kms " + ans%1000 + " meter(s)");
     }
 
     @Override
